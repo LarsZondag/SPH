1ed7a315139be4687655ec4405d28f463069c01b
LarsZondag <larszondag@gmail.com>
2016-04-27 15:24:59 +0200

    Working code. Need to test parameters

--- code/python/lars.py
+++ code/python/lars.py
@@ -3,28 +3,43 @@ import matplotlib.pyplot as plt
 from code.python.cubicSpline import CubicSpline
 from numba import jit
 
-N = 50
-dim = 2
-c2 = 1
-gamma = 1
-density_0 = 10
-h = 4#1.45
-lx = 0.4 * h
-ly = 0.4 * h
-m = 1
-pos = np.random.random((N, dim))
-pos[:, 0] *= lx
-pos[:, 1] *= ly
-kernel = CubicSpline(dim, h)
 
+dim = 2
+c2 = 300
+gamma = 7
+density_0 = 100#8
+h = 0.1
+lx = 25
+ly = 25
+m = 1
+N_x = 20
+N_y = 20
+N = N_x * N_y
+pos = np.zeros((N_x * N_y, dim))
+randoms = (np.random.random((N, dim)) - 0.5) * 2 * 0.1
+
+for i in range(N_x):
+    for j in range(N_y):
+        pos[i * N_y + j, 0] = i + randoms[i * N_y + j, 0]
+        pos[i * N_y + j, 1] = j + randoms[i * N_y + j, 1]
+pos[:,0] /= N_x
+pos[:,1] /= N_y
+
+pos[:, 0] *= 0.5 * lx
+pos[:, 1] *= 0.5 * ly
+pos[:, 0] += 0.25 * lx
+pos[:, 1] += 0.1 * ly
+kernel = CubicSpline(dim, h)
+v_0 = 10
 vel = np.zeros((N, dim))
 acc = np.zeros((N, dim))
 
 dt = 0.004
 N_t = 2000
 picture_time = 5
-g = np.array([0, -1])
-mu = 4
+g = np.array([0, -9.81])
+mu = 0
+nu = 1
 
 @jit
 def calc_distances_and_density(positions):
@@ -45,7 +60,6 @@ def calc_distances_and_density(positions):
 @jit
 def calc_acc(distance_vec, distance_abs, density, velocities):
     pressure = density_0 * c2 / gamma * ((density / density_0) ** gamma - 1)
-    # print(np.average(pressure))
     array_length = len(density)
     dimensions = len(distance_vec[0, 0, :])
     acceleration = np.zeros((array_length, dimensions))
@@ -54,13 +68,14 @@ def calc_acc(distance_vec, distance_abs, density, velocities):
         for j in range(i + 1, array_length):
             if distance_abs[i, j] < 2 * h:
                 pres_tmp = pressure[i] / density[i] ** 2 + pressure[j] / density[j] ** 2
-                if np.dot(velocities[j] - velocities[i], distance_vec[i, j]) > 0:
+                if np.dot(velocities[j] - velocities[i], distance_vec[i, j]) < 0:
                     visc_tmp = 2 * (-c2 * mu + 2 * mu * mu) / (density[i] + density[j])
                 else:
                     visc_tmp = 0
-                tmp = m * (pres_tmp - visc_tmp) * kernel.gradient(distance_vec[i, j], distance_abs[i, j])
-                acceleration[i] -= tmp
-                acceleration[j] += tmp
+                tmp = - m * (pres_tmp + visc_tmp) * kernel.gradient(distance_vec[i, j], distance_abs[i, j])
+                acceleration[i] += tmp
+                acceleration[j] -= tmp
+    acceleration -= nu * velocities
     return acceleration
 
 
@@ -72,6 +87,7 @@ def make_time_step(positions, velocities):
     bdry_cdt = np.floor(positions/lx) * np.mod(positions, lx)
     positions -= 2 * bdry_cdt
     distances_vec, distances_abs, density = calc_distances_and_density(positions)
+    # print(np.average(density))
     accelerations = calc_acc(distances_vec, distances_abs, density, velocities)
     velocities += (accelerations + g) * dt
     velocities[bdry_cdt != 0] *= - np.sign(bdry_cdt[bdry_cdt != 0])
